5fc7ec53de5a911bbd542f76249ec84100ff0a24
RomainLAU <romain.laurent@epitech.digital>
2022-05-21 19:13:01 +0200

    Adding the price convertion for donations

---
+++ public/assets/scripts/beezConvertion.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    amountInput = document.querySelector('#amount')
+    convertionValue = document.querySelector('#convertion')
+
+    amountInput.addEventListener('input', () => {
+        convertedValue = (parseFloat(amountInput.value) * 3.33)/100
+        console.log((parseFloat(amountInput.value) * 3.33).toFixed(2), parseFloat(amountInput.value))
+
+        if (isNaN(convertedValue) === false) {
+            convertionValue.textContent = convertedValue.toFixed(2)
+        } else {
+            convertionValue.textContent = ''
+        }
+    })
+})
